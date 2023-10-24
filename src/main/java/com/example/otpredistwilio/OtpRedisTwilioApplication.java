38bdb4332a56031ba18dbc056e4d70062d799fa3
gyda13 <gyda2100@gmail.com>
2023-10-24 16:42:48 +0300

    Initial commit

---
+++ src/main/java/com/example/otpredistwilio/OtpRedisTwilioApplication.java
@@ -0,0 +1,13 @@
+package com.example.otpredistwilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpRedisTwilioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpRedisTwilioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/otpredistwilio/OtpRedisTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otpredistwilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpRedisTwilioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
