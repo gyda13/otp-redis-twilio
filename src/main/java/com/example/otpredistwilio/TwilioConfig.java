53f952c01eb56f4ffdc7b387a6a714a646be8af4
gyda13 <gyda2100@gmail.com>
2023-10-24 17:45:09 +0300

    adding redis and twilio config

---
+++ src/main/java/com/example/otpredistwilio/TwilioConfig.java
@@ -0,0 +1,29 @@
+package com.example.otpredistwilio;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TwilioConfig {
+
+    @Value("${twilio.account-sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth-token}")
+    private String authToken;
+
+    @Value("${twilio.phone-number}")
+    private String phoneNumber;
+
+    public String getAccountSid() {
+        return accountSid;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
