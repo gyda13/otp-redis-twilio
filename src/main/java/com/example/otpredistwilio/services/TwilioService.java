687258e4c47bc8bf3d8df796fd0ff2b8ea86b201
gyda13 <gyda2100@gmail.com>
2023-10-25 16:22:09 +0300

    adding twilio request service

---
+++ src/main/java/com/example/otpredistwilio/services/TwilioService.java
@@ -0,0 +1,28 @@
+package com.example.otpredistwilio.services;
+
+import com.example.otpredistwilio.TwilioConfig;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TwilioService {
+
+    private final TwilioConfig twilioConfig;
+
+    @Autowired
+    public TwilioService(TwilioConfig twilioConfig) {
+        this.twilioConfig = twilioConfig;
+        Twilio.init(twilioConfig.getAccountSid(), twilioConfig.getAuthToken());
+    }
+
+    public void sendOTP(String phoneNumber, String otpCode) {
+        Message message = Message.creator(
+                        new PhoneNumber(phoneNumber),
+                        new PhoneNumber(twilioConfig.getPhoneNumber()),
+                        "Your OTP is: " + otpCode)
+                .create();
+    }
+}
